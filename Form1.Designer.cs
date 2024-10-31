f0fb234b1db4ec0fb90a4934c5d2bba8497db366
nguyenly124 <nguyenthily124da@gmail.com>
2024-10-31 16:07:38 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace giao_diện_bán_hàng
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridViewsanpham = new DataGridView();
+            buttonthem = new Button();
+            buttonxoa = new Button();
+            buttonthanhtoan = new Button();
+            dataGridViewgiohang = new DataGridView();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            txtthanhtien = new TextBox();
+            label2 = new Label();
+            txtsoluong = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewsanpham).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewgiohang).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridViewsanpham
+            // 
+            dataGridViewsanpham.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewsanpham.Location = new Point(6, 39);
+            dataGridViewsanpham.Name = "dataGridViewsanpham";
+            dataGridViewsanpham.RowHeadersWidth = 51;
+            dataGridViewsanpham.Size = new Size(514, 569);
+            dataGridViewsanpham.TabIndex = 0;
+            // 
+            // buttonthem
+            // 
+            buttonthem.Location = new Point(357, 636);
+            buttonthem.Name = "buttonthem";
+            buttonthem.Size = new Size(163, 29);
+            buttonthem.TabIndex = 1;
+            buttonthem.Text = "Thêm vào giỏ hàng";
+            buttonthem.UseVisualStyleBackColor = true;
+            buttonthem.Click += buttonthem_Click;
+            // 
+            // buttonxoa
+            // 
+            buttonxoa.Location = new Point(225, 506);
+            buttonxoa.Name = "buttonxoa";
+            buttonxoa.Size = new Size(174, 29);
+            buttonxoa.TabIndex = 2;
+            buttonxoa.Text = "Xóa khỏi giỏ hàng ";
+            buttonxoa.UseVisualStyleBackColor = true;
+            buttonxoa.Click += button2_Click;
+            // 
+            // buttonthanhtoan
+            // 
+            buttonthanhtoan.Location = new Point(833, 688);
+            buttonthanhtoan.Name = "buttonthanhtoan";
+            buttonthanhtoan.Size = new Size(142, 29);
+            buttonthanhtoan.TabIndex = 3;
+            buttonthanhtoan.Text = "Thanh toán ";
+            buttonthanhtoan.UseVisualStyleBackColor = true;
+            buttonthanhtoan.Click += buttonthanhtoan_Click;
+            // 
+            // dataGridViewgiohang
+            // 
+            dataGridViewgiohang.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewgiohang.Location = new Point(6, 39);
+            dataGridViewgiohang.Name = "dataGridViewgiohang";
+            dataGridViewgiohang.RowHeadersWidth = 51;
+            dataGridViewgiohang.Size = new Size(393, 452);
+            dataGridViewgiohang.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridViewsanpham);
+            groupBox1.Controls.Add(buttonthem);
+            groupBox1.Location = new Point(16, 17);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(535, 682);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Danh sách sản phẩm ";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(txtthanhtien);
+            groupBox2.Controls.Add(label2);
+            groupBox2.Controls.Add(txtsoluong);
+            groupBox2.Controls.Add(label1);
+            groupBox2.Controls.Add(dataGridViewgiohang);
+            groupBox2.Controls.Add(buttonxoa);
+            groupBox2.Location = new Point(566, 17);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(409, 665);
+            groupBox2.TabIndex = 6;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Giỏ hàng ";
+            groupBox2.Enter += groupBox2_Enter;
+            // 
+            // txtthanhtien
+            // 
+            txtthanhtien.Location = new Point(122, 600);
+            txtthanhtien.Name = "txtthanhtien";
+            txtthanhtien.Size = new Size(245, 27);
+            txtthanhtien.TabIndex = 10;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 607);
+            label2.Name = "label2";
+            label2.Size = new Size(82, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Thành tiền ";
+            // 
+            // txtsoluong
+            // 
+            txtsoluong.Location = new Point(122, 559);
+            txtsoluong.Name = "txtsoluong";
+            txtsoluong.Size = new Size(245, 27);
+            txtsoluong.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 559);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Số lượng ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1006, 729);
+            Controls.Add(groupBox2);
+            Controls.Add(buttonthanhtoan);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewsanpham).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewgiohang).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dataGridViewsanpham;
+        private Button buttonthem;
+        private Button buttonxoa;
+        private Button buttonthanhtoan;
+        private DataGridView dataGridViewgiohang;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label2;
+        private TextBox txtsoluong;
+        private Label label1;
+        private TextBox txtthanhtien;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+using System.Security.Policy;
+
+namespace giao_diện_bán_hàng
+{
+    public partial class Form1 : Form
+    {
+
+        // Danh sách các sản phẩm (Ví dụ)
+        List<SanPham> danhsach = new List<SanPham>();
+
+        // Giỏ hàng
+        List<SanPham> giohang = new List<SanPham>();
+        public Form1()
+        {
+            InitializeComponent();
+            danhsach.Add(new SanPham { Ten = "Sản phẩm A", Hinhanh = " ", Gia = 10000, Soluong = 5 });
+            danhsach.Add(new SanPham { Ten = "Sản phẩm B", Hinhanh = " ", Gia = 20000, Soluong = 4 });
+            danhsach.Add(new SanPham { Ten = "Sản phẩm C", Hinhanh = " ", Gia = 40000, Soluong = 2 });
+            danhsach.Add(new SanPham { Ten = "Sản phẩm D", Hinhanh = " ", Gia = 5000, Soluong = 1 });
+            danhsach.Add(new SanPham { Ten = "Sản phẩm E", Hinhanh = " ", Gia = 300000, Soluong = 7 });
+
+            // Hiển thị danh sách sản phẩm lên DataGridView
+            dataGridViewsanpham.DataSource = danhsach;
+
+        }
+
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Lấy index của hàng đang được chọn trong DataGridView giỏ hàng
+            int rowIndex = dataGridViewgiohang.CurrentRow.Index;
+
+            // Lấy sản phẩm cần xóa
+            SanPham sanPhamXoa = giohang[rowIndex];
+            // Tìm sản phẩm tương ứng trong danh sách sản phẩm
+            int indexSanPhamTrongDanhSach = danhsach.FindIndex(sp => sp.Ten == sanPhamXoa.Ten);
+            if (sanPhamXoa.Soluong > 1) { sanPhamXoa.Soluong--; }
+            else
+            {
+                // Xóa sản phẩm khỏi giỏ hàng
+                giohang.RemoveAt(rowIndex);
+            }
+            // Nếu tìm thấy sản phẩm, tăng số lượng lên 1
+            if (indexSanPhamTrongDanhSach != -1)
+            {
+                danhsach[indexSanPhamTrongDanhSach].Soluong++;
+
+                // Cập nhật lại DataGridView danh sách sản phẩm
+                dataGridViewsanpham.Refresh();
+            }
+
+            // Cập nhật lại DataGridView giỏ hàng
+            dataGridViewgiohang.DataSource = null;
+            dataGridViewgiohang.DataSource = giohang;
+            TinhTong();
+        }
+
+        private void buttonthem_Click(object sender, EventArgs e)
+        {
+            var rowIndex = dataGridViewsanpham.CurrentRow.Index;
+            var sanPham = danhsach[rowIndex];
+            if (sanPham.Soluong == 0)
+            {
+                MessageBox.Show("Sản phẩm đã đạt số lượng tối đa !", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // Thoát khỏi hàm nếu số lượng bằng 0
+            }
+            SanPham sanPhamGioHang = new SanPham
+            {
+                Ten = sanPham.Ten,
+                Hinhanh = sanPham.Hinhanh,
+                Gia = sanPham.Gia,
+                Soluong = 1
+            };
+            int indexsptronggio = giohang.FindIndex(sp => sp.Ten == sanPham.Ten);
+
+            // Nếu tìm thấy sản phẩm, tăng số lượng lên 1
+            if (indexsptronggio != -1)
+            {
+                giohang[indexsptronggio].Soluong++;
+
+            }
+            else
+            {
+                // Thêm sản phẩm vào giỏ hàng
+                giohang.Add(sanPhamGioHang);
+
+            }
+
+            // Giảm số lượng sản phẩm trong danh sách
+            sanPham.Soluong--;
+            // Cập nhật lại DataGridView của giỏ hàng
+            dataGridViewgiohang.DataSource = null;
+            dataGridViewgiohang.DataSource = giohang;
+
+            // Cập nhật lại DataGridView
+            dataGridViewsanpham.Refresh();
+            // Tính toán tổng
+            TinhTong();
+        }
+        private void TinhTong()
+        {
+            int tongSoLuong = 0;
+            decimal tongGiaTri = 0;
+
+            foreach (SanPham sanPham in giohang)
+            {
+                tongSoLuong += 1;
+                tongGiaTri += sanPham.Gia * sanPham.Soluong;
+            }
+
+            // Cập nhật giao diện (ví dụ: sử dụng các label)
+            txtsoluong.Text = tongSoLuong.ToString();
+            txtthanhtien.Text = tongGiaTri.ToString("C"); // Định dạng tiền tệ
+
+        }
+
+        private void buttonthanhtoan_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thanh toán ? ", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+    if (result == DialogResult.Yes)
+            {
+                // Xóa giỏ hàng
+                giohang.Clear();
+                  // Cập nhật giao diện
+                dataGridViewgiohang.DataSource = null;
+                txtthanhtien.Text = "0";
+                txtsoluong.Text = "0";
+
+                MessageBox.Show("Đơn hàng của bạn đã được đặt thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+
+}
+public class SanPham
+{
+    public string Ten { get; set; }
+    public string Hinhanh { get; set; }
+    public decimal Gia { get; set; }
+
+    public int Soluong {  get; set; }
+}
+
